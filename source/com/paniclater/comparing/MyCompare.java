5f348b58afbe33e3241b512ceae48a89fa29bd54
paniclater <paniclater@gmail.com>
2013-12-16 11:04:24 -0700

    MyCompare implements comparable and overrides the compareTo(Object o) method and can be sorted via Collections.sort()

---
+++ source/com/paniclater/comparing/MyCompare.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MyCompare implements Comparable<MyCompare> {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	MyCompare(String name) {
+		this.name = name;
+	}
+	public int compareTo(MyCompare a) {
+		return name.compareTo(a.getName());
+	}
+	public static void main(String[] args) {
+		MyCompare a = new MyCompare("Bobby");
+		MyCompare b = new MyCompare("Andrea");
+		List<MyCompare> list = new ArrayList<MyCompare>();
+		list.add(a);
+		list.add(b);
+		Collections.sort(list);
+		for(MyCompare c : list) { System.out.println(c.getName()); }
+	}	
+}
