21f96f3c4abd989f76675802141690358aca5b7b
paniclater <paniclater@gmail.com>
2013-12-19 13:15:11 -0700

    Another practice code section I didn't understand from a test question

---
+++ source/com/paniclater/threads/Chess.java
@@ -0,0 +1,16 @@
+public class Chess implements Runnable {
+	public void run() {
+		move(Thread.currentThread().getId());
+	}
+	//Insert code here
+	synchronized void move(long id) {
+	//void move(long id) {
+		System.out.print(id + " ");
+		System.out.print(id + " ");
+	}
+	public static void main(String[] args) {
+		Chess ch = new Chess();
+		new Thread(ch).start();
+		new Thread(new Chess()).start();
+	}
+}
