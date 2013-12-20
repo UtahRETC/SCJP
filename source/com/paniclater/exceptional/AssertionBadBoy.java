a59595b71208521339f9773fc9d43f7814f7ab5a
paniclater <paniclater@gmail.com>
2013-12-20 15:45:02 -0700

    run AssertionBadBoy with java -ea to see an Assertion Error in the wild.

---
+++ source/com/paniclater/exceptional/AssertionBadBoy.java
@@ -0,0 +1,11 @@
+public class AssertionBadBoy {
+	int x = 0;
+	//never catch errors.
+	void checkX() {
+		assert x == 1;
+	}
+	public static void main(String[] args) {
+		AssertionBadBoy abb = new AssertionBadBoy();
+		abb.checkX();	
+	}	
+}
