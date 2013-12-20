843963b71730c9afff6c7f3690fc3e7c329d2d45
paniclater <paniclater@gmail.com>
2013-12-20 15:13:02 -0700

    NumberFormatBadBoy demonstrates generating a NumberFormatException with the Integer.valueOf(String s) method. It also shows catching the exception.

---
+++ source/com/paniclater/exceptional/NumberFormatBadBoy.java
@@ -0,0 +1,12 @@
+public class NumberFormatBadBoy {
+	public static void main(String[] args) {
+		String s = "5";
+		Integer i = Integer.valueOf(s);
+		try {
+			s = "Five";
+			i = Integer.valueOf(s);
+		} catch (NumberFormatException nfe) {
+			System.out.println(nfe);
+		}
+	}		
+}
